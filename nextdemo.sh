7c5804559152365b5e0e8afd258899b829a34fc4
akashcto <akash.agrawal3@wipro.com>
2014-05-02 06:05:06 -0600

    Update nextdemo.sh

--- nextdemo.sh
+++ nextdemo.sh
@@ -1,4 +1,4 @@
-docker kill chef-server
-docker kill $(docker ps | grep " pandrew/chef-client:latest")
-docker run -d -i -t -h testchefserver.wipro.com -name "chef_server" -p 0.0.0.0:443:443 -p 0.0.0.0:4568:4568 -p 0.0.0.0:5570:5570 wipro/ocd-chefserver:0.1.0 /bin/bash
+sudo docker kill chef-server
+sudo docker kill $(sudo docker ps | grep " pandrew/chef-client:latest")
+sudo docker run -d -i -t -h testchefserver.wipro.com -name "chef_server" -p 0.0.0.0:443:443 -p 0.0.0.0:4568:4568 -p 0.0.0.0:5570:5570 wipro/ocd-chefserver:0.1.0 /bin/bash
 sleep 120
