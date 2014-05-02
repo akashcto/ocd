466542e699ecad167c44f7f3b8af531da0a7378a
akashcto <akash.agrawal3@wipro.com>
2014-05-02 08:01:02 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo docker run -d -i -t -p 0.0.0.0:8081:8081 wipro/ocd-nexus:0.1.0 bash start.sh
-sudo docker run -d -i -t -p 0.0.0.0:8080:8080 wipro/ocd-jenkins:0.1.0 bash start.sh
+sudo docker run -d -i -t -p 0.0.0.0:8080:8080 -p 0.0.0.0:9001:9001 wipro/ocd-jenkins:0.1.0 bash start.sh
 sleep 60
 sudo docker run -d -i -t -p 0.0.0.0:5556:5556 -p 0.0.0.0:5557:5557 -p 0.0.0.0:3335:3335 -p 0.0.0.0:8899:8899 -p 0.0.0.0:4445:4445 -p 0.0.0.0:5559:5559 -p 0.0.0.0:6666:6666 -p 0.0.0.0:6665:6665 -p 0.0.0.0:5561:5561 -p 0.0.0.0:7770:7770 -p 0.0.0.0:7779:7779 wipro/ocd-orchestrator:0.1.0 bash start.sh
 sudo docker run -d -i -t -p 0.0.0.0:5566:5566 -p 0.0.0.0:5568:5568 -p 0.0.0.0:5560:5560 -p 0.0.0.0:7789:7789 -p 0.0.0.0:5555:5555 wipro/ocd-javaservice:0.1.0 bash start.sh
