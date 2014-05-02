31f98cd47e5bf969b44dd8660f5130142fc7dea1
akashcto <akash.agrawal3@wipro.com>
2014-05-02 07:33:55 -0600

    Update Main.sh

--- Main.sh
+++ Main.sh
@@ -8,7 +8,7 @@ sleep 10
 sudo docker run -d -i -t -h testchefserver.wipro.com -name "chef_server" -p 0.0.0.0:443:443 -p 0.0.0.0:4568:4568 -p 0.0.0.0:5570:5570 wipro/ocd-chefserver:0.1.0 bash start.sh
 sleep 120
 sudo mkdir /etc/chef-server/
-sudo docker cp chef-server:/etc/chef-server/chef-validator.pem /etc/chef-server/
+sudo docker cp chef_server:/etc/chef-server/chef-validator.pem /etc/chef-server/
 sudo docker tag wipro/ocd-chefclient:0.1.0 pandrew/chef-client
 cd /home/ubuntu
 mkdir ocd1/
