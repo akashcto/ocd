1841f32487309a658c7e543cc1585fe59d319eb9
akashcto <akash.agrawal3@wipro.com>
2014-05-03 20:02:04 +0530

    Update create.sh

--- create.sh
+++ create.sh
@@ -7,5 +7,12 @@ cd Dockerfiles
 sudo make
 if ["$?" -ne "0" ]; then
   echo "Make Failed"
-  exit 1
+  exit 0
 fi
+echo "hello"
+sudo rm /var/lib/docker/linkgraph.db
+sudo rm /var/run/docker.pid
+sleep 10
+sudo docker -d & disown
+sleep 10
+sudo make
